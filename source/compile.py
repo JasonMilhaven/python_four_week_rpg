5365468b1ff9a54616bc232367203d9de1aa9555
brainddoser <jasonmilhaven@gmail.com>
2017-03-07 17:07:26 -0500

    idk

--- source/compile.py
+++ source/compile.py
@@ -3,10 +3,27 @@ import os
 import shutil
 import compileall
 
-DIR = os.getcwd().rsplit("\\", 1)[0] + "\\Python_Four_Week_Rpg\\"
+from utilities import *
+
+DIR = get_dir()
 DIR_SRC = DIR + "source\\"
 DIR_CACHE = DIR_SRC + "__pycache__\\"
 DIR_BUILD = DIR + "build\\"
+
+"""
+	******************************************************************************
+
+	Module: compile
+	
+	Description: Is called from a batch file, will compile the project source into
+	bytecode, which can be launched from launch.exe.
+	
+	Author: Jason Milhaven
+	
+	History: 
+	
+	******************************************************************************
+"""
 
 if os.path.exists(DIR_BUILD):
 	shutil.rmtree(DIR_BUILD)
